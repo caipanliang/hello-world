2ab33d2dff904f2bf33cbadb2f7f9900cf40ddbe
caipanliang <cpl6560@163.com>
2017-05-08 22:08:42 +0800

    Add hello world method by java

---
+++ HelloWord.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.web.cn;
+
+/**
+ * <p>
+ * 
+ * </p>
+ * @author [Tiny Slient] 2017年5月8日下午10:07:04
+ * @version V1.0
+ * @modificationHistory=========================逻辑或功能性重大变更记录
+ * @modify by user: 
+ * @modify by reason:
+ */
+public class HelloWord {
+
+	/**
+	 * <p>
+	 * 
+	 * </p>
+	 * @author [Tiny Slient] 2017年5月8日下午10:07:04
+	 * @version V1.0
+	 * @modificationHistory=========================逻辑或功能性重大变更记录
+	 * @modify by user: 
+	 * @modify by reason:
+	 */
+	public static void main(String[] args) {
+		/**
+		 * <p>
+		 * 
+		 * </p>
+		 * @author [Tiny Slient] 2017年5月8日下午10:07:04
+		 * @version V1.0
+		 * @modificationHistory=========================逻辑或功能性重大变更记录
+		 * @modify by user: 
+		 * @modify by reason:
+		 */
+		System.out.println("++++Hello Word>>>>");
+	}
+	/**
+	 * <p>
+	 * 
+	 * </p>
+	 * @author [Tiny Slient] 2017年5月8日下午10:07:04
+	 * @version V1.0
+	 * @modificationHistory=========================逻辑或功能性重大变更记录
+	 * @modify by user: 
+	 * @modify by reason:
+	 */
+}
